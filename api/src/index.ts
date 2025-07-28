4ce41f633e5ba229e69b023bd55c2b99c5905ace
vadimP74 <mydom74@gmail.com>
2025-07-28 15:51:48 -0500

    Products Router Setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import productsRouter from './routes/products/index';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World 123');
-})
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.use('/products', productsRouter);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
