be978d62d262931306bf2bf71833127471ef0790
vadimP74 <mydom74@gmail.com>
2025-07-29 01:20:53 -0500

    Validation with zod + drizzle schema

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -6,14 +6,27 @@ import {
   createProduct,
   deleteProduct,
 } from './productsController';
+import { validateData } from '../../middlewares/validationMiddleware';
+import {
+  createProductSchema,
+  updateProductSchema,
+} from '../../db/productsSchema';
+
+// import { z } from 'zod';
+
+// const createProductSchema = z.object({
+//   name: z.string(),
+//   price: z.number({ message: 'Price should be a number.'}),
+// })
+
 
 const router = Router();
 
 // products endpoints
 router.get('/', listProducts);
 router.get('/:id', getProductById);
-router.post('/', createProduct);
-router.put('/:id', updateProduct);
+router.post('/', validateData(createProductSchema), createProduct);
+router.put('/:id', validateData(updateProductSchema), updateProduct);
 router.delete('/:id', deleteProduct);
 
 export default router;
